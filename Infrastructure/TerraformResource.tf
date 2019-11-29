4a133adfca2f67ab9f6c0d75c1e456f278d258db
nivik13579 <40626882+nivik13579@users.noreply.github.com>
2019-11-29 17:27:29 +0530

    Added new tf

--- Infrastructure/TerraformResource.tf
+++ Infrastructure/TerraformResource.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "TFResource2019" {
-  name     = "TFResource2019RG"
+resource "azurerm_resource_group" "2TFResource2019" {
+  name     = "2TFResource2019RG"
   location = "West Europe"
 }
 
-resource "azurerm_app_service_plan" "TFResource2019" {
-  name                = "TFResource2019appserviceplan"
-  location            = "${azurerm_resource_group.TFResource2019.location}"
-  resource_group_name = "${azurerm_resource_group.TFResource2019.name}"
+resource "azurerm_app_service_plan" "2TFResource2019" {
+  name                = "2TFResource2019appserviceplan"
+  location            = "${azurerm_resource_group.2TFResource2019.location}"
+  resource_group_name = "${azurerm_resource_group.2TFResource2019.name}"
   kind                = "Linux"
   sku {
     tier = "Standard"
@@ -14,10 +14,10 @@ resource "azurerm_app_service_plan" "TFResource2019" {
   }
 }
 
-resource "azurerm_app_service" "TFResource2019" {
-  name                = "TFResource2019appservice"
-  location            = "${azurerm_resource_group.TFResource2019.location}"
-  resource_group_name = "${azurerm_resource_group.TFResource2019.name}"
+resource "azurerm_app_service" "2TFResource2019" {
+  name                = "2TFResource2019appservice"
+  location            = "${azurerm_resource_group.2TFResource2019.location}"
+  resource_group_name = "${azurerm_resource_group.2TFResource2019.name}"
   app_service_plan_id = "${azurerm_app_service_plan.TFResource2019.id}"
   site_config {
     java_version           = "1.8"
