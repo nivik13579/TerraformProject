d2be146c6eaf1087bcecddad125f2993a1ca10cb
nivik13579 <40626882+nivik13579@users.noreply.github.com>
2019-11-29 18:11:14 +0530

    Added new tf

--- Infrastructure/TerraformResource.tf
+++ Infrastructure/TerraformResource.tf
@@ -1,10 +1,10 @@
-resource "azurerm_resource_group" "2TFResource2019" {
-  name     = "2TFResource2019RG"
+resource "azurerm_resource_group" "TFResource20192" {
+  name     = "TFResource2019RG2"
   location = "West Europe"
 }
 
-resource "azurerm_app_service_plan" "2TFResource2019" {
-  name                = "2TFResource2019appserviceplan"
+resource "azurerm_app_service_plan" "TFResource20192" {
+  name                = "TFResource2019appserviceplan2"
   location            = "${azurerm_resource_group.2TFResource2019.location}"
   resource_group_name = "${azurerm_resource_group.2TFResource2019.name}"
   kind                = "Linux"
@@ -14,8 +14,8 @@ resource "azurerm_app_service_plan" "2TFResource2019" {
   }
 }
 
-resource "azurerm_app_service" "2TFResource2019" {
-  name                = "2TFResource2019appservice"
+resource "azurerm_app_service" "TFResource20192" {
+  name                = "TFResource2019appservice2"
   location            = "${azurerm_resource_group.2TFResource2019.location}"
   resource_group_name = "${azurerm_resource_group.2TFResource2019.name}"
   app_service_plan_id = "${azurerm_app_service_plan.TFResource2019.id}"
